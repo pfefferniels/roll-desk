045a42add35f28844b8b06a009e1a3c47703e9bd
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2022-07-22 20:32:53 +0200

    work towards #3

--- src/components/AlignmentEditor.tsx
+++ src/components/AlignmentEditor.tsx
@@ -3,6 +3,8 @@ import GlobalContext from "./GlobalContext"
 import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Paper, Slider, TextField, Typography } from "@mui/material"
 import { AlignmentPair } from "sequence-align/src/types"
 import { Motivation, SemanticAlignmentPair } from "../lib/AlignedPerformance"
+import { MidiNote } from "../lib/Performance"
+import { Note } from "../lib/Score"
 
 function midiPitchToNoteName(midiPitch: number): string {
   const notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
@@ -11,11 +13,12 @@ function midiPitchToNoteName(midiPitch: number): string {
 
 interface EditMotivationProps {
   pair?: SemanticAlignmentPair,
+  changeMotivation: (pair: SemanticAlignmentPair, target: Motivation) => void,
   dialogOpen: boolean,
   setDialogOpen: (open: boolean) => void
 }
 
-const EditMotivation: FC<EditMotivationProps> = ({ pair, dialogOpen, setDialogOpen }): JSX.Element => {
+const EditMotivation: FC<EditMotivationProps> = ({ pair, changeMotivation, dialogOpen, setDialogOpen }): JSX.Element => {
   return (
     <Dialog open={dialogOpen}>
       <DialogTitle>Edit Alignment Motivation</DialogTitle>
@@ -23,7 +26,10 @@ const EditMotivation: FC<EditMotivationProps> = ({ pair, dialogOpen, setDialogOp
         <div>current motivation: {pair?.motivation}</div>
       </DialogContent>
       <DialogActions>
-        <Button onClick={() => setDialogOpen(false)}>Save</Button>
+        <Button onClick={() => {
+          changeMotivation(pair!, Motivation.OctaveAlteration)
+          setDialogOpen(false)
+        }}>Save</Button>
       </DialogActions>
     </Dialog>
   )
@@ -36,8 +42,8 @@ export default function AlignmentEditor() {
   const { alignedPerformance, alignmentReady, triggerUpdate } = useContext(GlobalContext)
   const [editDialogOpen, setEditDialogOpen] = useState(false)
   const [currentAlignmentPair, setCurrentAlignmentPair] = useState<SemanticAlignmentPair>()
-  //const [activeScoreNote, setActiveScoreNote] = useState<string>('')
-  //const [activeMIDINote, setActiveMIDINote] = useState<string>('')
+  const [activeScoreNote, setActiveScoreNote] = useState<Note>()
+  const [activeMIDINote, setActiveMIDINote] = useState<MidiNote>()
 
   const changeGapOpen = (event: Event, newValue: number | number[]) => {
     alignedPerformance.setGapOpen(newValue as number)
@@ -47,6 +53,33 @@ export default function AlignmentEditor() {
   const changeGapExt = (event: Event, newValue: number | number[]) => {
     alignedPerformance.setGapExt(newValue as number)
     triggerUpdate()
+  }
+
+  const removeAlignment = (pair: SemanticAlignmentPair) => {
+    // remove the current alignment
+    const index = alignedPerformance.semanticPairs.indexOf(pair)
+    alignedPerformance.semanticPairs.splice(index, 1)
+
+    // insert two gap alignments
+    alignedPerformance.semanticPairs.push({
+      scoreNote: pair.scoreNote,
+      motivation: Motivation.Omission
+    })
+
+    alignedPerformance.semanticPairs.push({
+      midiNote: pair.midiNote,
+      motivation: Motivation.Addition
+    })
+
+    triggerUpdate()
+  }
+
+  const changeMotivation = (pair: SemanticAlignmentPair, target: Motivation) => {
+    const index = alignedPerformance.semanticPairs.indexOf(pair)
+    if (index >= 0) {
+      alignedPerformance.semanticPairs[index].motivation = target
+      triggerUpdate()
+    }
   }
 
   return (
@@ -83,12 +116,12 @@ export default function AlignmentEditor() {
         <svg width={2000} height={areaHeight + 300}>
           {alignedPerformance.getSemanticPairs().map((pair) => {
             const scoreNotePosition = pair.scoreNote ?
-                                        [pair.scoreNote.qstamp * horizontalStretch,
-                                         (127 - pair.scoreNote.pitch) * verticalStretch] : [,]
-            const midiNotePosition = pair.midiNote ? 
-                                        [pair.midiNote.onsetTime * horizontalStretch,
-                                         (127 - pair.midiNote.pitch) * verticalStretch + areaHeight] : [,]
-            
+              [pair.scoreNote.qstamp * horizontalStretch,
+              (127 - pair.scoreNote.pitch) * verticalStretch] : [,]
+            const midiNotePosition = pair.midiNote ?
+              [pair.midiNote.onsetTime * horizontalStretch,
+              (127 - pair.midiNote.pitch) * verticalStretch + areaHeight] : [,]
+
             return (
               <g>
                 {pair.scoreNote && (
@@ -100,8 +133,22 @@ export default function AlignmentEditor() {
                     width={pair.scoreNote.duration * horizontalStretch}
                     height={7}
                     onClick={(e) => {
-                      if (e.altKey) {
-                        // TODO
+                      setActiveScoreNote(pair.scoreNote)
+                      if (activeMIDINote) {
+                        // remove the unaligned MIDI note from the alignments array
+                        const index = alignedPerformance.semanticPairs.findIndex(other => other.midiNote === pair.midiNote)
+                        alignedPerformance.semanticPairs.splice(index, 1)
+
+                        // find the unaligned score note and attach the new MIDI note to it 
+                        // as an exact match
+                        const pairToModify = alignedPerformance.semanticPairs.find(other => other.scoreNote === pair.scoreNote)
+                        if (pairToModify) {
+                          pairToModify.midiNote = activeMIDINote
+                          pairToModify.motivation = Motivation.ExactMatch
+                        }
+                        triggerUpdate()
+                        setActiveMIDINote(undefined)
+                        setActiveScoreNote(undefined)
                       }
                     }} />
                 )}
@@ -115,25 +162,41 @@ export default function AlignmentEditor() {
                     width={pair.midiNote.duration * horizontalStretch}
                     height={7}
                     onClick={(e) => {
-                      if (e.altKey) {
-                        // TODO
+                      setActiveMIDINote(pair.midiNote)
+                      if (activeScoreNote) {
+                        // remove the unaligned score note from the alignments array
+                        const index = alignedPerformance.semanticPairs.findIndex(other => other.scoreNote === pair.scoreNote)
+                        alignedPerformance.semanticPairs.splice(index, 1)
+
+                        // find the unaligned MIDI note and attach the new score note to it 
+                        // as an exact match
+                        const pairToModify = alignedPerformance.semanticPairs.find(other => other.midiNote === pair.midiNote)
+                        if (pairToModify) {
+                          pairToModify.scoreNote = activeScoreNote
+                          pairToModify.motivation = Motivation.ExactMatch
+                        }
+                        triggerUpdate()
+                        setActiveMIDINote(undefined)
+                        setActiveScoreNote(undefined)
                       }
                     }} />
                 )}
 
-                {pair.motivation === Motivation.ExactMatch && (
+                {(pair.midiNote && pair.scoreNote) && (
                   <line x1={scoreNotePosition[0]}
-                        y1={scoreNotePosition[1]}
-                        x2={midiNotePosition[0]}
-                        y2={midiNotePosition[1]}
-                        stroke="black"
-                        strokeWidth={2}
-                        strokeOpacity={0.8}
-                        onClick={() => {
-                          setCurrentAlignmentPair(pair)
-                          setEditDialogOpen(true)
-                        }}
-                        />
+                    y1={scoreNotePosition[1]}
+                    x2={midiNotePosition[0]}
+                    y2={midiNotePosition[1]}
+                    stroke={pair.motivation === Motivation.ExactMatch ? 'black' : 'blue'}
+                    strokeWidth={2}
+                    strokeOpacity={0.8}
+                    onClick={(e) => {
+                      if (e.altKey) removeAlignment(pair)
+                      else {
+                        setCurrentAlignmentPair(pair)
+                        setEditDialogOpen(true)
+                      }
+                    }} />
                 )}
               </g>
             )
@@ -143,6 +206,7 @@ export default function AlignmentEditor() {
       )}
 
       <EditMotivation pair={currentAlignmentPair}
+        changeMotivation={changeMotivation}
         dialogOpen={editDialogOpen}
         setDialogOpen={setEditDialogOpen} />
 
