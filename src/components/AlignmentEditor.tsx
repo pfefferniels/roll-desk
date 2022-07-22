e8cd3e37d4019121c9dc35956d814d9e1b3e372c
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2022-07-22 15:41:44 +0200

    display score to performance alignment

--- src/components/AlignmentEditor.tsx
+++ src/components/AlignmentEditor.tsx
@@ -5,10 +5,13 @@ import { AlignmentPair } from "sequence-align/src/types"
 
 function midiPitchToNoteName(midiPitch: number): string {
   const notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
-  return `${notes[midiPitch % 12]}${Math.floor(midiPitch/12)-1}`
+  return `${notes[midiPitch % 12]}${Math.floor(midiPitch / 12) - 1}`
 }
 
 export default function AlignmentEditor() {
+  const horizontalStretch = 40
+  const verticalStretch = 2
+  const areaHeight = 127 * verticalStretch + 50
   const scoreRef = useRef(null)
   const { alignedPerformance, alignmentReady, triggerUpdate } = useContext(GlobalContext)
 
@@ -23,95 +26,97 @@ export default function AlignmentEditor() {
   }
 
   useEffect(() => {
-    if (!alignmentReady) return
+    if (!scoreRef.current) return
 
-    console.log('update triggered')
+    const svgElement = (scoreRef.current as SVGElement)
+    svgElement.querySelectorAll('.connectionLine').forEach(line => line.remove())
 
-    if (scoreRef.current) {
-      (scoreRef.current as HTMLElement).innerHTML = alignedPerformance.score!.asSVG()
-    }
-
-    document.querySelector("svg")?.querySelectorAll("[fill]").forEach((value: Element) => value.removeAttribute("fill"))
-    
     alignedPerformance.getAllPairs().map((pair: AlignmentPair<string>, index, arr) => {
-      const ref: HTMLElement = scoreRef.current as unknown as HTMLElement || null
-      if (ref) {
-        console.log(pair[0], '->', pair[1])
-          if (pair[1] === '-') { // insertion
-            //el?.setAttribute('fill', 'blue')
-            return
-          }
-
-          const el = ref.querySelector('#' + pair[1])
-          if (!el) return
-
-          if (pair[0] === '-') { // deletion
-            el.setAttribute('fill', 'red')
-          }
-          else {
-            if (alignedPerformance.performedNoteAtIndex(Number(pair[0]))?.pitch !==
-                alignedPerformance.noteAtId(pair[1])?.pitch) {
-              el.setAttribute('fill', 'orange')
-            }
-            else {
-              el.setAttribute('fill', 'green')
-              el.setAttribute('data-alignment', pair[0])
-            }
-          }
+      if (pair[1] === '-') { // insertion
+        const note = svgElement.querySelector(`#midiNote_${pair[0]}`)
+        if (note) {
+          note.setAttribute('fill', 'red')
+        }
+      }
+      if (pair[0] === '-') { // deletion
+        const note = svgElement.querySelector(`#scoreNote_${pair[1]}`)
+        if (note) {
+          note.setAttribute('fill', 'red')
+        }
+      }
+      else {
+        const start = svgElement.querySelector(`#midiNote_${pair[0]}`) as SVGGraphicsElement
+        const end = svgElement.querySelector(`#scoreNote_${pair[1]}`) as SVGGraphicsElement
+        if (start && end) {
+          const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+          line.setAttribute('x1', start.getBBox().x.toString())
+          line.setAttribute('y1', start.getBBox().y.toString())
+          line.setAttribute('x2', end.getBBox().x.toString())
+          line.setAttribute('y2', end.getBBox().y.toString())
+          line.setAttribute('stroke', 'black')
+          line.setAttribute('class', 'connectionLine')
+          svgElement.appendChild(line)
+        }
       }
     })
-  }, [alignmentReady])
+  })
 
   return (
-      <div>
-        {alignedPerformance.ready() && (
-          <Paper style={{position: 'fixed', padding: '0.5rem'}}>
-            <Typography gutterBottom>Penalty for opening a new gap</Typography>
-            <Slider
-              getAriaLabel={() => 'gap open score'}
-              value={alignedPerformance.gapOpen}
-              onChange={changeGapOpen}
-              valueLabelDisplay="auto"
-              min={-15}
-              max={0}
-              marks={true}
-            />
+    <div>
+      {alignedPerformance.ready() && (
+        <Paper style={{ position: 'fixed', padding: '0.5rem', bottom: 0 }}>
+          <Typography gutterBottom>Penalty for opening a new gap</Typography>
+          <Slider
+            getAriaLabel={() => 'gap open score'}
+            value={alignedPerformance.gapOpen}
+            onChange={changeGapOpen}
+            valueLabelDisplay="auto"
+            min={-15}
+            max={0}
+            marks={true}
+          />
 
-            <Typography gutterBottom>penalty for extending an existing gap</Typography>
-            <Slider
-              getAriaLabel={() => 'gap ext score'}
-              value={alignedPerformance.gapExt}
-              onChange={changeGapExt}
-              valueLabelDisplay="auto"
-              min={-15}
-              max={0}
-              marks={true}
-            />
+          <Typography gutterBottom>penalty for extending an existing gap</Typography>
+          <Slider
+            getAriaLabel={() => 'gap ext score'}
+            value={alignedPerformance.gapExt}
+            onChange={changeGapExt}
+            valueLabelDisplay="auto"
+            min={-15}
+            max={0}
+            marks={true}
+          />
 
-            <Button variant='outlined'>Export MEI</Button>
-          </Paper>
-        )}
+          <Button variant='outlined'>Export MEI</Button>
+        </Paper>
+      )}
 
-        <div className="scoreDisplay" ref={scoreRef}/>
-        <div className="midiDisplay" style={{display: 'block'}}>
-          { alignedPerformance.rawPerformance &&
-            alignedPerformance.rawPerformance.asNotes().map((note => (
-              <div key={`note_${note.id}`} style={{float: 'left', margin: '0.2rem', border: '1px solid black'}}>
-                <div>id: {note.id}</div>
-                <div>onset time: {note.onsetTime}</div>
-                <div>pitch: {midiPitchToNoteName(note.pitch)}</div>
-              </div>
-            )))
-          }
-        </div>
+      {alignedPerformance.ready() && (
+        <svg width={2000} height={areaHeight + 200} ref={scoreRef}>
+          <g className="scoreDisplay">
+            {alignedPerformance.score &&
+              alignedPerformance.score.allNotes().map(note => (
+                <rect key={`note_${note.id}`} className='scoreNote' id={`scoreNote_${note.id}`} x={note.qstamp * horizontalStretch} y={note.pitch * verticalStretch} width={note.duration * horizontalStretch} height={5}>
+                  <desc>pitch: {midiPitchToNoteName(note.pitch)}</desc>
+                </rect>
+              ))
+            }
+          </g>
 
-        <div className='alignmentDisplay' style={{display: 'block', width: '50vw'}}>
-          {alignmentReady && 
-          alignedPerformance.getAllPairs().map((pair => (
-            <div>{pair[0]} – {pair[1]}</div>
-          )))
-          }
-        </div>
-      </div>
+          <g className="midiDisplay">
+            {alignedPerformance.rawPerformance &&
+              alignedPerformance.rawPerformance.asNotes().map((note => (
+                <rect key={`note_${note.id}`} className='midiNote' id={`midiNote_${note.id}`} x={note.onsetTime*horizontalStretch} y={note.pitch * verticalStretch + areaHeight} width={note.duration*horizontalStretch} height={5}>
+                  <desc>pitch: {midiPitchToNoteName(note.pitch)}</desc>
+                </rect>
+              )))
+            }
+          </g>
+
+          <g className='alignmentDisplay'>
+          </g>
+        </svg>
+      )}
+    </div>
   )
 }
