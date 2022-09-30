5e9dce8094e8dd23863f04ba1a546cf25f3d33b0
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2022-09-30 21:50:39 +0200

    provide RDF store

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import './App.css';
 import { SpeedDial, SpeedDialAction, SpeedDialIcon, Tab, Tabs, Typography } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
@@ -10,8 +10,10 @@ import AnnotatedScore from './components/AnnotatedScore';
 import GlobalContext from './components/GlobalContext';
 import Upload from './components/Upload'
 import { AlignedPerformance } from './lib/AlignedPerformance';
+import { Store, graph } from 'rdflib'
+import { RdfStoreContext } from './RDFStoreContext';
 
-function TabPanel(props: {children: ReactElement, index: string, value: string}) {
+function TabPanel(props: { children: ReactElement, index: string, value: string }) {
   const { children, value, index } = props;
 
   return (
@@ -34,10 +36,12 @@ function App() {
   const [alignedPerformance, setAlignedPerformance] = useState(new AlignedPerformance())
   const [alignmentReady, setAlignmentReady] = useState<number>(0)
 
+  const [rdfStore, setRdfStore] = useState<Store>(graph())
+
   const closeUploadDialog = () => setUploadDialogOpen(false)
   const openUploadDialog = () => setUploadDialogOpen(true)
 
-  const triggerUpdate = () => setAlignmentReady(alignmentReady+1)
+  const triggerUpdate = () => setAlignmentReady(alignmentReady + 1)
 
   const actions = [
     { icon: <SaveIcon />, name: 'Save' },
@@ -46,53 +50,58 @@ function App() {
 
   return (
     <div className="App">
-      <GlobalContext.Provider value={{
-        alignedPerformance, 
-        alignmentReady,
-        triggerUpdate
+      <RdfStoreContext.Provider value={rdfStore && {
+        rdfStore: rdfStore
       }}>
-        <Tabs value={activeTab} onChange={(e, nv) => setActiveTab(nv)} aria-label="main tabs">
-          <Tab value='alignment-editor' label="Alignment Editor" />
-          <Tab value='interpolation-editor' label="Interpolation Editor"/>
-          <Tab value='annotated-score' label="Annotated Score"/>
-        </Tabs>
-        <TabPanel value={activeTab} index='alignment-editor'>
-          <AlignmentEditor />
-        </TabPanel>
-        <TabPanel value={activeTab} index='interpolation-editor'>
-          <InterpolationEditor/>
-        </TabPanel>
-        <TabPanel value={activeTab} index='annotated-score'>
-          <AnnotatedScore/>
-        </TabPanel>
 
-        <SpeedDial  
-          ariaLabel="modify workspace"  
-          sx={{ position: 'fixed', bottom: 16, right: 16 }}
-          icon={<SpeedDialIcon />}
-        >
-          {actions.map((action) => (
-            <SpeedDialAction
-              key={action.name}
-              icon={action.icon}
-              tooltipTitle={action.name}
-              onClick={action.action}
-            />
-          ))}
-        </SpeedDial>
+        <GlobalContext.Provider value={{
+          alignedPerformance,
+          alignmentReady,
+          triggerUpdate
+        }}>
+          <Tabs value={activeTab} onChange={(e, nv) => setActiveTab(nv)} aria-label="main tabs">
+            <Tab value='alignment-editor' label="Alignment Editor" />
+            <Tab value='interpolation-editor' label="Interpolation Editor" />
+            <Tab value='annotated-score' label="Annotated Score" />
+          </Tabs>
+          <TabPanel value={activeTab} index='alignment-editor'>
+            <AlignmentEditor />
+          </TabPanel>
+          <TabPanel value={activeTab} index='interpolation-editor'>
+            <InterpolationEditor />
+          </TabPanel>
+          <TabPanel value={activeTab} index='annotated-score'>
+            <AnnotatedScore />
+          </TabPanel>
 
-        <Upload
-          open={uploadDialogOpen}
-          onClose={closeUploadDialog}
-          setScore={(newScore) => {
-            alignedPerformance.setScore(newScore)
-            triggerUpdate()
-          }}
-          setPerformance={(newPerformance) => {
-            alignedPerformance.setPerformance(newPerformance)
-            triggerUpdate()
-          }} />
-      </GlobalContext.Provider>
+          <SpeedDial
+            ariaLabel="modify workspace"
+            sx={{ position: 'fixed', bottom: 16, right: 16 }}
+            icon={<SpeedDialIcon />}
+          >
+            {actions.map((action) => (
+              <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+                onClick={action.action}
+              />
+            ))}
+          </SpeedDial>
+
+          <Upload
+            open={uploadDialogOpen}
+            onClose={closeUploadDialog}
+            setScore={(newScore) => {
+              alignedPerformance.setScore(newScore)
+              triggerUpdate()
+            }}
+            setPerformance={(newPerformance) => {
+              alignedPerformance.setPerformance(newPerformance)
+              triggerUpdate()
+            }} />
+        </GlobalContext.Provider>
+      </RdfStoreContext.Provider>
     </div>
   );
 }
---
+++ src/RDFStoreContext.tsx
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+import { Store } from 'rdflib';
+
+
+export interface RDFStoreInterface {
+  rdfStore: Store;
+}
+
+export const RdfStoreContext = createContext<RDFStoreInterface | null>(null);
