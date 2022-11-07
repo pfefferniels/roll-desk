f2038b0e0867c377aa8087c3213f2300f3e1ee5e
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2022-11-07 11:01:20 +0100

    add transition test

--- tests/InterpolateTempoMap.test.ts
+++ tests/InterpolateTempoMap.test.ts
@@ -11,8 +11,8 @@ describe('InterpolateTempoMap', () => {
         instruction given a MIDI file with a constant tempo`, async () => {
         // Arrange
         const msm = await prepareMSM(
-            readFileSync('tests/files/neutral-tempo/score.mei', 'utf16le'),
-            readFileSync('tests/files/neutral-tempo/neutral.mid'))
+            readFileSync('tests/files/tempo/neutral.mei', 'utf16le'),
+            readFileSync('tests/files/tempo/neutral.mid'))
         const mpm = new MPM()
 
         // Act
@@ -27,6 +27,40 @@ describe('InterpolateTempoMap', () => {
             date: 0,
             beatLength: 0.25,
             bpm: 32
+        }])
+    })
+
+    it(`Correctly interpolates tempo transitions`, async () => {
+        // Arrange
+        const msm = await prepareMSM(
+            readFileSync('tests/files/tempo/neutral.mei', 'utf16le'),
+            readFileSync('tests/files/tempo/transition.mid'),
+            readFileSync('tests/files/tempo/transition-alignment.jsonld', 'utf-8'))
+        const mpm = new MPM()
+
+        // Act
+        const transformer = new InterpolateTempoMap()
+        transformer.transform(msm, mpm)
+
+        // Assert
+        // This MIDI file was generated with a linear tempo transition from 
+        // quarters = 30 to quarters = 60
+        const tempoInstructions = mpm.getInstructions<Tempo>('tempo', 'global')
+        expect(tempoInstructions).toEqual([{
+            'xml:id': expect.any(String),
+            type: 'tempo',
+            date: 0,
+            beatLength: 0.25,
+            bpm: 30,
+            'transition.to': 60,
+            meanTempoAt: 0.5
+        },
+        {
+            'xml:id': expect.any(String),
+            type: 'tempo',
+            date: 1440,
+            beatLength: 0.25,
+            bpm: 60
         }])
     })
 })
