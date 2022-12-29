1d9f3a661378ea6cccde6f65583d18845846eaf8
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2022-12-29 17:57:02 +0100

    sketch RDF export

--- src/lib/visitors/RDFVisitor.ts
+++ src/lib/visitors/RDFVisitor.ts
@@ -4,6 +4,15 @@ import { AlignedPerformance } from "../AlignedPerformance";
 import { RawPerformance } from "../midi/RawPerformance";
 import { Visitor } from "./Visitor";
 
+/**
+ * Capitalizes the event type and append 'event' to it
+ * @param eventType 
+ * @returns string
+ */
+const normalizeEventType = (eventType: string) => {
+    return eventType[0].toUpperCase() + eventType.slice(1) + 'Event'
+}
+
 export class RdfVisitor implements Visitor {
     private _datasets: SolidDataset[]
 
@@ -35,7 +44,7 @@ export class RdfVisitor implements Visitor {
      * @returns a `SolidDataset` containing the `Thing`s representing the MIDI file
      */
     visitPerformance(performance: RawPerformance): SolidDataset {
-        const dataset = createSolidDataset()
+        let dataset = createSolidDataset()
         const midi = performance.midi
         if (!midi) return dataset
 
@@ -43,25 +52,36 @@ export class RdfVisitor implements Visitor {
             .addUrl(RDF.type, 'http://purl.org/midi-ld/midi#Piece')
             .addInteger('http://purl.org/midi-ld/midi#resolution', midi.header.ticksPerBeat)
             .addInteger('http://purl.org/midi-ld/midi#format', midi.header.formatType)
-        
+
         for (const midiTrack of midi.tracks) {
             const track = buildThing(createThing())
                 .addUrl(RDF.type, 'http://purl.org/midi-ld/midi#Track')
-            
+
             for (const midiEvent of midiTrack) {
                 const event = buildThing(createThing())
-                    .addUrl(RDF.type, `http://purl.org/midi-ld/midi#${midiEvent.type}`)
-                    .build()
-                track.addUrl('http://purl.org/midi-ld/midi#hasEvent', event.url)
+                    .addUrl(RDF.type, `http://purl.org/midi-ld/midi#${normalizeEventType(midiEvent.type)}`)
+                    .addInteger('http://purl.org/midi-ld/midi#tick', midiEvent.deltaTime)
+
+                if (midiEvent.type === 'channel') {
+                    event
+                        .addInteger('http://purl.org/midi-ld/midi#channel', midiEvent.channel)
+                        .addUrl(RDF.type, `http://purl.org/midi-ld/midi#${normalizeEventType(midiEvent.subtype)}`)
+
+                    if (midiEvent.subtype === 'noteOn') {
+                        event
+                            .addInteger('http://purl.org/midi-ld/midi#pitch', midiEvent.noteNumber)
+                            .addInteger('http://purl.org/midi-ld/midi#velocity', midiEvent.velocity)
+                    }
+                }
+                dataset = setThing(dataset, event.build())
+                track.addUrl('http://purl.org/midi-ld/midi#hasEvent', event.build().url)
             }
+            dataset = setThing(dataset, track.build())
             piece.addUrl('http://purl.org/midi-ld/midi#hasTrack', track.build().url)
         }
+        dataset = setThing(dataset, piece.build())
 
-        const modifiedDataset = setThing(dataset, piece.build())
-        console.log('modified dataset=', modifiedDataset)
-
-        this._datasets.push(modifiedDataset)
-
+        this._datasets.push(dataset)
         return dataset
     }
 }
