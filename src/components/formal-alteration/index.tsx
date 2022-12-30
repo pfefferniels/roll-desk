e4dbe7e2046ac87fb329542a223e3ed9711e11d4
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2022-12-30 17:16:48 +0100

    load MEI into verovio

--- src/components/formal-alteration/index.tsx
+++ src/components/formal-alteration/index.tsx
@@ -1,18 +1,60 @@
-import { Thing } from "@inrupt/solid-client"
+import { getContentType, getFile, getSourceUrl, getUrl, isRawData, Thing } from "@inrupt/solid-client"
+import { useSession } from "@inrupt/solid-ui-react"
+import { RDFS } from "@inrupt/vocab-common-rdf"
 import { PlayArrowRounded } from "@mui/icons-material"
 import { Button, IconButton, Typography } from "@mui/material"
 import { useState } from "react"
+import { loadVerovio } from "../../lib/globals"
 import { FindEntity } from "../network-overview/FindEntity"
 
 type SelectionMode = 'score' | 'midi'
 
+/**
+ * The formal alteration editor allows to specify how a performer 
+ * alters the formal structure of a score. It will insert a bunch
+ * of `FormalAlteration` entities into the graph and create a new 
+ * MEI file with <section>s and <expansion> added to the original 
+ * file.
+ */
 export const FormalAlterationEditor = () => {
+    const { session } = useSession()
     const [entityFinderOpen, setEntityFinderOpen] = useState(false)
     const [selectionMode, setSelectionMode] = useState<SelectionMode>()
+
+    const [svg, setSVG] = useState()
 
     const handleFindEntity = (type: SelectionMode) => {
         setSelectionMode(type)
         setEntityFinderOpen(true)
+    }
+
+    const renderMEI = async (thing: Thing) => {
+        const fileURL = getUrl(thing, RDFS.label)
+        if (!fileURL) return
+
+        try {
+            const file = await getFile(fileURL, { fetch: session.fetch })
+            if (!isRawData(file) || getContentType(file) !== 'application/xml') {
+                console.warn('The resource should not be a dataset and should have the MIME type "application/xml"')
+            }
+
+            // load the MEI file into verovio
+            const meiData = await file.text()
+            const vrvToolkit = await loadVerovio()
+            vrvToolkit.setOptions({
+                pageHeight: document.body.clientHeight,
+                pageWidth: document.body.clientWidth,
+                adjustPageHeight: true,
+                svgHtml5: true
+            })
+            vrvToolkit.loadData(meiData)
+
+            // render to SVG and save it as a state
+            const svg = vrvToolkit.renderToSVG(1)
+            setSVG(svg)
+        } catch (err) {
+            console.warn(err)
+        }
     }
 
     return (
@@ -31,9 +73,7 @@ export const FormalAlterationEditor = () => {
                 <PlayArrowRounded />
             </IconButton>
 
-            <div>
-                Score
-            </div>
+            <div style={{width: '80vw'}} dangerouslySetInnerHTML={{__html: svg || ''}} />
 
             <div>
                 <h3>Alterations Overview</h3>
@@ -48,7 +88,7 @@ export const FormalAlterationEditor = () => {
                         // Use thing to play it
                     }
                     else if (selectionMode === 'score') {
-                        // Use thing to display score
+                        renderMEI(thing)
                     }
                 }} />
         </div>
