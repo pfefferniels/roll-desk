f14a3ef0525197c16474c0a8e395dedf28d54414
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-05-21 23:32:41 +0200

    change boundary to square bracket

--- src/components/midi-ld/Boundary.tsx
+++ src/components/midi-ld/Boundary.tsx
@@ -6,26 +6,68 @@ interface BoundaryProps {
     noteHeight: number;
 }
 
-export const Boundary = ({ begin, end, pitch, pixelsPerTick, noteHeight }: BoundaryProps) => (
-    <>
-        <line // Dotted line for the beginning
-            x1={begin * pixelsPerTick}
-            y1={(128 - pitch) * noteHeight}
-            x2={begin * pixelsPerTick}
-            y2={(128 - pitch) * noteHeight + noteHeight}
-            stroke='black'
-            strokeWidth={1}
-            strokeDasharray={1}
-        />
+export const Boundary = ({ begin, end, pitch, pixelsPerTick, noteHeight }: BoundaryProps) => {
+    const bracketLength = 2.5; // Length of the horizontal lines for brackets
 
-        <line // Dotted line for the end
-            x1={end * pixelsPerTick}
-            y1={(128 - pitch) * noteHeight}
-            x2={end * pixelsPerTick}
-            y2={(128 - pitch) * noteHeight + noteHeight}
-            stroke='black'
-            strokeWidth={1}
-            strokeDasharray={1}
-        />
-    </>
-);
+    return (
+        <>
+            <g className='startBoundary'>
+                <line // Vertical line for the beginning
+                    x1={begin * pixelsPerTick}
+                    y1={(128 - pitch) * noteHeight}
+                    x2={begin * pixelsPerTick}
+                    y2={(128 - pitch) * noteHeight + noteHeight}
+                    stroke='black'
+                    strokeWidth={1}
+                />
+
+                <line // Top horizontal line for the beginning bracket
+                    x1={begin * pixelsPerTick}
+                    y1={(128 - pitch) * noteHeight}
+                    x2={begin * pixelsPerTick + bracketLength}
+                    y2={(128 - pitch) * noteHeight}
+                    stroke='black'
+                    strokeWidth={1}
+                />
+
+                <line // Bottom horizontal line for the beginning bracket
+                    x1={begin * pixelsPerTick}
+                    y1={(128 - pitch) * noteHeight + noteHeight}
+                    x2={begin * pixelsPerTick + bracketLength}
+                    y2={(128 - pitch) * noteHeight + noteHeight}
+                    stroke='black'
+                    strokeWidth={1}
+                />
+            </g>
+
+            <g className='endBoundary'>
+                <line // Vertical line for the end
+                    x1={end * pixelsPerTick}
+                    y1={(128 - pitch) * noteHeight}
+                    x2={end * pixelsPerTick}
+                    y2={(128 - pitch) * noteHeight + noteHeight}
+                    stroke='black'
+                    strokeWidth={1}
+                />
+
+                <line // Top horizontal line for the ending bracket
+                    x1={end * pixelsPerTick}
+                    y1={(128 - pitch) * noteHeight}
+                    x2={end * pixelsPerTick - bracketLength}
+                    y2={(128 - pitch) * noteHeight}
+                    stroke='black'
+                    strokeWidth={1}
+                />
+
+                <line // Bottom horizontal line for the ending bracket
+                    x1={end * pixelsPerTick}
+                    y1={(128 - pitch) * noteHeight + noteHeight}
+                    x2={end * pixelsPerTick - bracketLength}
+                    y2={(128 - pitch) * noteHeight + noteHeight}
+                    stroke='black'
+                    strokeWidth={1}
+                />
+            </g>
+        </>
+    );
+};
