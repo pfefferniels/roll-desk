69a6324713f4a549ef44f6a1e362f50e9367f912
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-05-22 00:17:36 +0200

    stubs for drag on boundary

--- src/components/midi-ld/Boundary.tsx
+++ src/components/midi-ld/Boundary.tsx
@@ -1,3 +1,6 @@
+import * as d3 from 'd3'
+import { useEffect, useRef, useState } from 'react';
+
 interface BoundaryProps {
     begin: number;
     end: number;
@@ -9,60 +12,85 @@ interface BoundaryProps {
 export const Boundary = ({ begin, end, pitch, pixelsPerTick, noteHeight }: BoundaryProps) => {
     const bracketLength = 2.5; // Length of the horizontal lines for brackets
 
+    const refStartBoundary = useRef<SVGGElement | null>(null);
+    const refEndBoundary = useRef<SVGGElement | null>(null);
+
+    const [startPos, setStartPos] = useState(begin * pixelsPerTick);
+    const [endPos, setEndPos] = useState(end * pixelsPerTick);
+
+    useEffect(() => {
+        refStartBoundary.current && d3.select(refStartBoundary.current)
+            .call(
+                (d3.drag()
+                    .on("drag", (event: any) => setStartPos(event.x))
+                    .on("end", dragend)) as any
+            );
+
+        refEndBoundary.current && d3.select(refEndBoundary.current)
+            .call(
+                (d3.drag()
+                    .on("drag", (event: any) => setEndPos(event.x))
+                    .on("end", dragend)) as any
+            );
+    }, []);
+
+    const dragend = (event: any) => {
+    };
+
     return (
         <>
-            <g className='startBoundary'>
+            <g className='startBoundary' ref={refStartBoundary} transform={`translate(${startPos},0)`}>
                 <line // Vertical line for the beginning
-                    x1={begin * pixelsPerTick}
+                    x1={0}
                     y1={(128 - pitch) * noteHeight}
-                    x2={begin * pixelsPerTick}
+                    x2={0}
                     y2={(128 - pitch) * noteHeight + noteHeight}
                     stroke='black'
                     strokeWidth={1}
                 />
 
                 <line // Top horizontal line for the beginning bracket
-                    x1={begin * pixelsPerTick}
+                    x1={0}
                     y1={(128 - pitch) * noteHeight}
-                    x2={begin * pixelsPerTick + bracketLength}
+                    x2={bracketLength}
                     y2={(128 - pitch) * noteHeight}
                     stroke='black'
                     strokeWidth={1}
                 />
 
                 <line // Bottom horizontal line for the beginning bracket
-                    x1={begin * pixelsPerTick}
+                    x1={0}
                     y1={(128 - pitch) * noteHeight + noteHeight}
-                    x2={begin * pixelsPerTick + bracketLength}
+                    x2={bracketLength}
                     y2={(128 - pitch) * noteHeight + noteHeight}
                     stroke='black'
                     strokeWidth={1}
                 />
             </g>
 
-            <g className='endBoundary'>
+            <g className='endBoundary' ref={refEndBoundary} transform={`translate(${endPos},0)`}>
                 <line // Vertical line for the end
-                    x1={end * pixelsPerTick}
+                    x1={0}
                     y1={(128 - pitch) * noteHeight}
-                    x2={end * pixelsPerTick}
+                    x2={0}
                     y2={(128 - pitch) * noteHeight + noteHeight}
                     stroke='black'
                     strokeWidth={1}
                 />
 
                 <line // Top horizontal line for the ending bracket
-                    x1={end * pixelsPerTick}
+                    x1={0}
                     y1={(128 - pitch) * noteHeight}
-                    x2={end * pixelsPerTick - bracketLength}
+                    x2={-bracketLength}
                     y2={(128 - pitch) * noteHeight}
                     stroke='black'
                     strokeWidth={1}
                 />
 
                 <line // Bottom horizontal line for the ending bracket
-                    x1={end * pixelsPerTick}
+                    x1={0}
                     y1={(128 - pitch) * noteHeight + noteHeight}
-                    x2={end * pixelsPerTick - bracketLength}
+                    x2={-bracketLength}
                     y2={(128 - pitch) * noteHeight + noteHeight}
                     stroke='black'
                     strokeWidth={1}
