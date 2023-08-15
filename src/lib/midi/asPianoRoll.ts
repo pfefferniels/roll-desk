c7f73ea47b2a6f59e5118db0a64df26cf6ab2995
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-08-15 22:30:54 +0200

    fix calculation of absolute note times

--- src/lib/midi/asPianoRoll.ts
+++ src/lib/midi/asPianoRoll.ts
@@ -1,5 +1,5 @@
 import { SolidDataset, Thing, asUrl, getInteger, getThing, getUrl, getUrlAll } from "@inrupt/solid-client"
-import { crm, mer, midi } from "../../helpers/namespaces"
+import { mer, midi } from "../../helpers/namespaces"
 import { RDF } from "@inrupt/vocab-common-rdf"
 import { PianoRoll, pitchToSitch } from "alignmenttool"
 
@@ -8,9 +8,8 @@ const tempoToTickTime = (event: Thing, ticksPerBeat: number) => {
     const microsecondsPerBeat = getInteger(event, midi('microsecondsPerBeat'))
     if (!microsecondsPerBeat) return 0
 
-    const BPM = 60000000 / microsecondsPerBeat
-    const realTimePerBeat = 60 / BPM
-    return realTimePerBeat / ticksPerBeat
+    const realTimePerBeat = microsecondsPerBeat / 1000000; // Convert to seconds
+    return realTimePerBeat / ticksPerBeat;
 }
 
 
@@ -22,10 +21,56 @@ const tempoToTickTime = (event: Thing, ticksPerBeat: number) => {
  * @returns a `SolidDataset` containing the `Thing`s representing the MIDI file
  */
 export const asPianoRoll = (piece: Thing, midiDataset: SolidDataset): PianoRoll | null => {
+    const sortEvents = (a?: Thing | null, b?: Thing | null) => {
+        if (!a || !b) return 0
+
+        let tickA = getInteger(a, midi('absoluteTick'))
+        let tickB = getInteger(b, midi('absoluteTick'))
+
+        if (getUrlAll(a, RDF.type).includes(mer('NoteEvent')) && !tickA) {
+            const onsetUrl = getUrl(a, mer('has_onset'))
+            if (onsetUrl) {
+                const onset = getThing(midiDataset, onsetUrl)
+                tickA = (onset && getInteger(onset, midi('absoluteTick')))
+            }
+        }
+
+        if (getUrlAll(b, RDF.type).includes(mer('NoteEvent')) && !tickB) {
+            const onsetUrl = getUrl(b, mer('has_onset'))
+            if (onsetUrl) {
+                const onset = getThing(midiDataset, onsetUrl)
+                tickB = (onset && getInteger(onset, midi('absoluteTick')))
+            }
+        }
+
+        return (tickA || 0) - (tickB || 0)
+    }
+
+
     const ticksPerBeat = getInteger(piece, midi('resolution')) || 480
 
     const allEvents = []
     const trackUrls = getUrlAll(piece, midi('hasTrack'))
+
+    // Get and sort all `SetTempoEvent`s
+    const tempos: { tick: number, tempo: number }[] = []
+    for (const trackUrl of trackUrls) {
+        const trackThing = getThing(midiDataset, trackUrl)
+        if (!trackThing) continue
+
+        const eventUrls = getUrlAll(trackThing, midi('hasEvent'))
+        const eventThings = eventUrls
+            .map(url => getThing(midiDataset, url))
+            .filter(event => event !== null && getUrlAll(event, RDF.type).includes(midi('SetTempoEvent')))
+            .map(event => ({
+                tick: getInteger(event!, midi('absoluteTick')) || 0,
+                tempo: tempoToTickTime(event!, ticksPerBeat)
+            }))
+
+        tempos.push(...eventThings)
+    }
+    tempos.sort((a, b) => a.tick - b.tick)
+
     for (const trackUrl of trackUrls) {
         const trackThing = getThing(midiDataset, trackUrl)
         if (!trackThing) continue
@@ -34,44 +79,12 @@ export const asPianoRoll = (piece: Thing, midiDataset: SolidDataset): PianoRoll 
         const eventThings = eventUrls
             .map(url => getThing(midiDataset, url))
             .filter(event => event !== null)
-            .sort((a, b) => {
-                if (!a || !b) return 0
-
-                let tickA = getInteger(a, midi('absoluteTick')) || 0
-                let tickB = getInteger(b, midi('absoluteTick')) || 0
-
-                if (getUrlAll(a, RDF.type).includes(mer('NoteEvent')) && !tickA) {
-                    const onsetUrl = getUrl(a, mer('has_onset'))
-                    if (onsetUrl) {
-                        const onset = getThing(midiDataset, onsetUrl)
-                        tickA = (onset && getInteger(onset, midi('absoluteTick'))) || 0
-                    }
-                }
-
-                if (getUrlAll(b, RDF.type).includes(mer('NoteEvent')) && !tickB) {
-                    const onsetUrl = getUrl(b, mer('has_onset'))
-                    if (onsetUrl) {
-                        const onset = getThing(midiDataset, onsetUrl)
-                        tickB = (onset && getInteger(onset, midi('absoluteTick'))) || 0
-                    }
-                }
-
-
-                return tickA - tickB
-            })
-
-        let currentTickTime = 0
+            .sort(sortEvents)
 
         for (const event of eventThings) {
             if (!event) continue
 
-            if (getUrlAll(event, RDF.type).includes(midi('SetTempoEvent'))) {
-                currentTickTime = tempoToTickTime(event, ticksPerBeat)
-            }
-            else if (getUrlAll(event, RDF.type).includes(mer('NoteEvent'))) {
-                // TODO something is going wrong here, as currentTickTime seems
-                // to be 0 permanently.
-
+            if (getUrlAll(event, RDF.type).includes(mer('NoteEvent'))) {
                 const onsetUrl = getUrl(event, mer('has_onset'))
                 const offsetUrl = getUrl(event, mer('has_offset'))
                 if (!onsetUrl || !offsetUrl) continue
@@ -82,6 +95,7 @@ export const asPianoRoll = (piece: Thing, midiDataset: SolidDataset): PianoRoll 
 
                 const onsetTime = getInteger(onset, midi('absoluteTick')) || 0
                 const offsetTime = getInteger(offset, midi('absoluteTick')) || 0
+                const tempo = tempos.reverse().find(tempo => tempo.tick <= onsetTime)?.tempo || 0
 
                 const pitch = getInteger(onset, midi('pitch')) || 0
 
@@ -89,20 +103,21 @@ export const asPianoRoll = (piece: Thing, midiDataset: SolidDataset): PianoRoll 
                 const offsetVelocity = getInteger(onset, midi('velocity')) || 0
 
                 allEvents.push({
-                    ontime: (currentTickTime || 0.001) * onsetTime,
-                    offtime: (currentTickTime || 0.001) * offsetTime,
+                    ontime: tempo * onsetTime,
+                    offtime: tempo * offsetTime,
                     id: asUrl(event),
                     pitch: pitch,
                     sitch: pitchToSitch(pitch),
                     onvel: onsetVelocity,
                     offvel: offsetVelocity,
                     channel: 0,
-                    endtime: (currentTickTime || 0.001) * offsetTime,
+                    endtime: tempo * offsetTime,
                     label: asUrl(event)
                 })
             }
         }
     }
+
     allEvents.sort((a, b) => a.ontime - b.ontime)
 
     const pr = new PianoRoll()
