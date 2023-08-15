26bb073de1414ef65d1f41d93f27ce9f26dfd7ec
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-08-15 23:38:38 +0200

    fix pipeline order

--- src/lib/transformers/defaultPipelines.ts
+++ src/lib/transformers/defaultPipelines.ts
@@ -17,15 +17,19 @@ export const getDefaultPipeline = (mode: 'melodic-texture' | 'chordal-texture', 
         return new Pipeline(
             new InterpolatePhysicalOrnamentation({ part: 0, minimumArpeggioSize: settings.minimumArpeggioSize, noteOffShiftTolerance: 250, placement: 'before-beat', durationThreshold: 10 }).setNext(
                 new InterpolatePhysicalOrnamentation({ part: 1, minimumArpeggioSize: settings.minimumArpeggioSize, noteOffShiftTolerance: 250, placement: 'on-beat', durationThreshold: 10 }).setNext(
-                    new InterpolateTempoMap({ beatLength: settings.beatLength, epsilon: 3, precision: 0 }).setNext(
-                        new InterpolateArticulation({ part: 0, relativeDurationPrecision: 1, relativeDurationTolerance: 0.1 }).setNext(
-                            new InterpolateArticulation({ part: 1, relativeDurationPrecision: 1, relativeDurationTolerance: 0.1 }).setNext(
-                                new InterpolateSymbolicOrnamentation().setNext(
-                                    new InterpolateDynamicsMap({ part: 0, beatLengthBasis: 'everything' }).setNext(
-                                        new InterpolateDynamicsMap({ part: 1, beatLengthBasis: 'everything' }).setNext(
-                                            new InterpolateAsynchrony({ part: 0, tolerance: 20, precision: 0 }).setNext(
-                                                new InterpolateTimingImprecision().setNext(
-                                                    new ExtractStyleDefinitions()
+                    new InterpolateAsynchrony({ part: 0, tolerance: 20, precision: 0 }).setNext(
+                        new InterpolateTempoMap({ beatLength: settings.beatLength, epsilon: 3, precision: 0 }).setNext(
+                            new InterpolateRubato({ part: 0, tolerance: 20 }).setNext(
+                                new InterpolateRubato({ part: 1, tolerance: 20 }).setNext(
+                                    new InterpolateArticulation({ part: 0, relativeDurationPrecision: 1, relativeDurationTolerance: 0.1 }).setNext(
+                                        new InterpolateArticulation({ part: 1, relativeDurationPrecision: 1, relativeDurationTolerance: 0.1 }).setNext(
+                                            new InterpolateSymbolicOrnamentation().setNext(
+                                                new InterpolateDynamicsMap({ part: 0, beatLengthBasis: 'everything' }).setNext(
+                                                    new InterpolateDynamicsMap({ part: 1, beatLengthBasis: 'everything' }).setNext(
+                                                        new InterpolateTimingImprecision().setNext(
+                                                            new ExtractStyleDefinitions()
+                                                        )
+                                                    )
                                                 )
                                             )
                                         )
