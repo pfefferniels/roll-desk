c69b558c783e543591d7f5a8c348d7fdadef1464
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-08-16 23:50:32 +0200

    move first test to vitest

--- src/lib/transformers/InterpolatePhysicalOrnamentation.test.ts
+++ src/lib/transformers/InterpolatePhysicalOrnamentation.test.ts
@@ -21,3 +21,38 @@ test('does not interpolate anything when no arpeggiation is given', () => {
     expect(ornamentInstruction.length).toEqual(0)
 })
 
+test('correctly interpolates real-word arpeggiations (WM 79)', () => {
+    // Arrange
+    const msm = parseMSM(
+        readFileSync('tests/files/arpeggiation/arpeggiated.msm', 'utf-8'))
+    const mpm = new MPM()
+
+    // Act
+    const transformer = new InterpolatePhysicalOrnamentation()
+    transformer.transform(msm, mpm)
+
+    // Assert
+    // In all the three bars, every chord is arpeggiated, which should result
+    // in 9 arpeggio instructions. Their lengthes are measured in Sonic Visualiser.
+    // The last two, however, are being played so quickly, that they fall
+    // under the default threshold.
+    const arpeggios = mpm.getInstructions<Ornament>('ornament', 'global')
+    expect(arpeggios).toHaveLength(7)
+    expect(arpeggios.map(a => a.frameLength)).toEqual([130, 35, 99, 310, 136, 188, 172 /* 12, 3 */])
+})
+
+test('calculates the noteoff.shift attribute', () => {
+    // Arrange
+    const msm = parseMSM(
+        readFileSync('tests/files/arpeggiation/noteoff.shift.msm', 'utf-8'))
+    const mpm = new MPM()
+
+    // Act
+    const transformer = new InterpolatePhysicalOrnamentation()
+    transformer.transform(msm, mpm)
+
+    // Assert
+    const ornamentInstructions = mpm.getInstructions<Ornament>('ornament', 'global')
+    expect(ornamentInstructions.map(o => o["noteoff.shift"])).toEqual(['true', 'false', 'monophonic'])
+})
+
