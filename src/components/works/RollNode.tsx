ab2bd8cded8cf8539f01731dfe3e50e0a570f4ad
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-10-22 23:53:26 +0200

    add some icons

--- src/components/works/RollNode.tsx
+++ src/components/works/RollNode.tsx
@@ -1,15 +1,30 @@
-import { Thing } from "@inrupt/solid-client"
+import { Thing, getStringNoLocale } from "@inrupt/solid-client"
+import { crm } from "../../helpers/namespaces"
+import { IconButton } from "@mui/material"
+import { AddOutlined, Remove } from "@mui/icons-material"
 
 interface RollNodeProps {
-    x: number 
-    y: number 
+    x: number
+    y: number
     thing: Thing
 }
 
 export const RollNode = ({ x, y, thing }: RollNodeProps) => {
     return (
-        <circle cx={x} cy={y} r={30} fill='red' fill-opacity={0.5}>
-            <text>Title: ...</text>
-        </circle>
+        <g className='rollNode'>
+            <circle cx={x} cy={y} r={50} fill='red' fill-opacity={0.2} />
+            <text x={x} y={y} fontSize={13} textAnchor="middle">{getStringNoLocale(thing, crm('P102_has_title'))}</text>
+            <foreignObject x={x - 50} y={y + 5} width={100} height={50}>
+                <div>
+                    <IconButton size="small">
+                        <AddOutlined />
+                    </IconButton>
+
+                    <IconButton size="small">
+                        <Remove />
+                    </IconButton>
+                </div>
+            </foreignObject>
+        </g>
     )
 }
