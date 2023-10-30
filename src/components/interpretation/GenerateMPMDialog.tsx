9de8ab4fda8809b9e13dcb98b7938b37b2939da0
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-10-30 14:09:56 +0100

    improve MPM generation dialog

--- src/components/interpretation/GenerateMPMDialog.tsx
+++ src/components/interpretation/GenerateMPMDialog.tsx
@@ -1,8 +1,8 @@
 import { Thing, getUrl, getThing, getFile, getSolidDataset, getUrlAll, buildThing, asUrl } from "@inrupt/solid-client"
-import { useSession, DatasetContext } from "@inrupt/solid-ui-react"
-import { RDFS, RDF } from "@inrupt/vocab-common-rdf"
-import { Dialog, DialogTitle, DialogContent, Box, Typography, DialogActions, Button, CircularProgress, Select, MenuItem, Stack } from "@mui/material"
-import { useContext, useState } from "react"
+import { useSession } from "@inrupt/solid-ui-react"
+import { RDF } from "@inrupt/vocab-common-rdf"
+import { Dialog, DialogTitle, DialogContent, Box, Typography, DialogActions, Button, CircularProgress, Select, MenuItem, Stack, Alert, Snackbar } from "@mui/material"
+import { useState } from "react"
 import { crm, mer, oa, crmdig } from "../../helpers/namespaces"
 import { urlAsLabel } from "../../helpers/urlAsLabel"
 import { loadVerovio } from "../../lib/loadVerovio.mjs"
@@ -22,9 +22,8 @@ interface GenerateMPMDialogProps {
 
 export const GenerateMPMDialog = ({ alignment, open, onCreate, onClose }: GenerateMPMDialogProps) => {
     const { session } = useSession()
-    const { solidDataset: dataset } = useContext(DatasetContext)
 
-    const [interpolationState, setInterpolationState] = useState<'fetching-mei' | 'fetching-midi' | 'transforming' | 'interpolating' | 'done'>()
+    const [message, setMessage] = useState<string>()
     const [defaultPipeline, setDefaultPipeline] = useState<'melodic-texture' | 'chordal-texture'>('melodic-texture')
     const [transformerSettings, setTransformerSettings] = useState<TransformerSettings>({
         minimumArpeggioSize: 2,
@@ -34,43 +33,49 @@ export const GenerateMPMDialog = ({ alignment, open, onCreate, onClose }: Genera
     })
 
     const performInterpolation = async () => {
-        if (!dataset) return
+        setMessage(`Performing interpolation`)
 
         const meiUrl = getUrl(alignment, mer('has_score'))
         const midiUrl = getUrl(alignment, mer('has_recording'))
-        if (!meiUrl || !midiUrl) return
+        console.log(meiUrl, midiUrl)
+        if (!meiUrl || !midiUrl) {
+            setMessage(`Invalid alignment provided.`)
+            return
+        }
 
-        const meiExpression = getThing(dataset, meiUrl)
-        const midiExpression = getThing(dataset, midiUrl)
-        if (!meiExpression || !midiExpression) return
-
-        setInterpolationState('fetching-mei')
+        setMessage(`Fetching MEI ${meiUrl}`)
         const mei = await getFile(
-            getUrl(meiExpression, RDFS.label) || '', { fetch: session.fetch as any })
+            meiUrl, { fetch: session.fetch as any })
         if (!mei) return
 
-        setInterpolationState('fetching-midi')
-        const pieceUrl = getUrl(midiExpression, RDFS.label)
-        const midiDataset = await getSolidDataset(
-            getUrl(midiExpression, RDFS.label) || '', { fetch: session.fetch as any }
-        )
-        if (!pieceUrl || !midiDataset) return
+        setMessage(`Fetching MIDI ${midiUrl}`)
+        const midiDataset = await getSolidDataset(midiUrl, { fetch: session.fetch as any })
+        if (!midiDataset) {
+            setMessage(`Failed loading Solid Dataset for MIDI ${midiUrl}`)
+            return
+        }
 
-        const piece = getThing(midiDataset, pieceUrl)
+        const piece = getThing(midiDataset, midiUrl)
         if (!piece) return
 
         const mei_ = new MEI(await mei.text(), await loadVerovio(), new DOMParser())
         const pr_ = asPianoRoll(piece, midiDataset)
         if (!pr_) return
 
-        setInterpolationState('transforming')
+        setMessage(`Transforming`)
 
         // convert alignment to MSM which then can be fed into the pipeline
         const msm = asMSM(mei_)
 
+        const alignmentDataset = await getSolidDataset(asUrl(alignment), { fetch: session.fetch as any })
+        if (!alignmentDataset) {
+            setMessage(`Failed loading Solid Dataset with alignment pairs for ${asUrl(alignment)}`)
+            return
+        }
+
         const pairUrls = getUrlAll(alignment, crm('P9_consists_of'))
         for (const pairUrl of pairUrls) {
-            const pair = getThing(dataset, pairUrl)
+            const pair = getThing(alignmentDataset, pairUrl)
             if (!pair) continue
 
             const scoreNoteId = urlAsLabel(getUrl(pair!, oa('hasTarget')))
@@ -83,20 +88,19 @@ export const GenerateMPMDialog = ({ alignment, open, onCreate, onClose }: Genera
             msm.addPerformanceInfo(scoreNoteId, midiNote)
         }
 
-
-        console.log(msm.serialize(false))
+        console.log('msm=', msm.serialize(false))
 
         const newMPM = new MPM(2)
 
-        setInterpolationState('interpolating')
         // kick-off pipeline
         getDefaultPipeline(defaultPipeline, transformerSettings).head?.transform(msm, newMPM)
 
-        setInterpolationState('done')
+        setMessage('Done.')
         return newMPM
     }
 
     const save = async () => {
+        setMessage(`Saving`)
         const creationEvent = buildThing()
             .addUrl(RDF.type, crmdig('D10_Software_Execution'))
             .addStringNoLocale(crmdig('L23_used_software_or_firmware'), 'mpm-interpolator')
@@ -110,44 +114,46 @@ export const GenerateMPMDialog = ({ alignment, open, onCreate, onClose }: Genera
         onCreate(creationEvent.build(), mpm)
     }
 
-    const workInProgress = interpolationState !== undefined && interpolationState !== 'done'
-    const ready = !workInProgress
+    const workInProgress = message !== undefined && message !== 'done'
 
     return (
-        <Dialog open={open} onClose={onClose}>
-            <DialogTitle>Perform MPM Interpolation</DialogTitle>
-            <DialogContent>
-                <Stack spacing={1}>
-                    <Box>
-                        <Typography>Which texture does the music have?</Typography>
-                        <Select
-                            sx={{ m: 1 }}
-                            size="small"
-                            value={defaultPipeline}
-                            onChange={e => setDefaultPipeline(e.target.value as 'melodic-texture' | 'chordal-texture')}>
-                            <MenuItem value='chordal-texture'>chordal</MenuItem>
-                            <MenuItem value='melodic-texture'>melodic</MenuItem>
-                        </Select>
-                    </Box>
-                    <TransformerSettingsBox onChange={(settings) => setTransformerSettings(settings)} />
-                </Stack>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose}>Cancel</Button>
-                <Button
-                    disabled={!ready}
-                    startIcon={workInProgress
-                        ? <CircularProgress />
-                        : <Save />}
-                    onClick={async () => {
-                        await save()
-                        onClose()
-                    }}
-                >
-                    {workInProgress ? interpolationState : 'Save'}
-                </Button>
-            </DialogActions>
-        </Dialog>
+        <>
+            <Snackbar message={message} open={!!message} />
+            <Dialog open={open} onClose={onClose}>
+                <DialogTitle>Generate MPM</DialogTitle>
+                <DialogContent>
+                    <Stack spacing={1}>
+                        <Alert severity="info">Existing MPM encodings will be overridden.</Alert>
+                        <Box>
+                            <Typography>Which texture does the music have?</Typography>
+                            <Select
+                                sx={{ m: 1 }}
+                                size="small"
+                                value={defaultPipeline}
+                                onChange={e => setDefaultPipeline(e.target.value as 'melodic-texture' | 'chordal-texture')}>
+                                <MenuItem value='chordal-texture'>chordal</MenuItem>
+                                <MenuItem value='melodic-texture'>melodic</MenuItem>
+                            </Select>
+                        </Box>
+                        <TransformerSettingsBox onChange={(settings) => setTransformerSettings(settings)} />
+                    </Stack>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClose}>Cancel</Button>
+                    <Button
+                        startIcon={workInProgress
+                            ? <CircularProgress />
+                            : <Save />}
+                        onClick={async () => {
+                            await save()
+                            onClose()
+                        }}
+                    >
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
 
