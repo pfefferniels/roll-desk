9160b94088fde9f8c5f075cc0de3214e9a272504
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-03-12 12:44:21 +0100

    add library mode

---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { RollCopyViewer } from "./roll-desk/RollCopyViewer"
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,52 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import * as path from 'path';
+import dts from 'vite-plugin-dts'
+
+// Detect whether we're building as a library or an app
+const isLib = process.env.BUILD_LIB === 'true';
 
 export default defineConfig(() => {
     return {
-        build: {
-            outDir: 'build',
-        },
+        build:
+            isLib
+                ? {
+                    lib: {
+                        entry: path.resolve(__dirname, 'src/components/index.ts'),
+                        formats: ['es']
+                    },
+                    rollupOptions: {
+                        external: ['react', 'react/jsx-runtime', '@mui/material', 'react-dom'],
+                        output: {
+                            globals: {
+                                react: 'React',
+                                'react-dom': 'ReactDOM',
+                            },
+                        },
+                    },
+                    copyPublicDir: false
+                }
+                : {
+                    outDir: 'build',
+                },
         plugins: [
             react(),
             eslint(),
-            wasm(),
-            topLevelAwait()
+            dts({
+                include: 'src/components',
+                tsconfigPath: path.resolve(__dirname, 'tsconfig.json'),
+            })
         ],
         worker: {
             format: 'es',
         },
         optimizeDeps: {
             include: ['@mui/material'],
-            exclude: ["alignmenttool", "verovio"],
             esbuildOptions: {
                 target: "es2020",
             },
+
         },
         server: {
             fs: {
