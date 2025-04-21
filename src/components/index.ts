068347ee14f8e3a3d28eae61854c5eb15e25cb8a
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-04-21 14:10:08 +0200

    make usable as library

--- src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { RollCopyViewer } from "./roll-desk/RollCopyViewer"
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { RollCopyViewer } from "./components/roll-desk/RollCopyViewer"
+export { WorkingPaper } from "./components/roll-desk/WorkingPaper"
+export { PinchZoomProvider } from "./hooks/usePinchZoom"
