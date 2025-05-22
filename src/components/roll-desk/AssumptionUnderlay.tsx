a82453f9c8751a9b5abc50d4edeac6f42a54e8a8
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-05-22 13:41:43 +0200

    move Hull to separate component

--- src/components/roll-desk/AssumptionUnderlay.tsx
+++ src/components/roll-desk/AssumptionUnderlay.tsx
@@ -1,8 +1,9 @@
-import { MouseEventHandler, ReactNode, RefObject, useState } from "react";
+import { RefObject } from "react";
 import { roundedHull } from "../../helpers/roundedHull";
 import { AnyEditorialAssumption } from "linked-rolls";
 import { getBoxToBoxArrow } from "curved-arrows";
 import { MultilineText } from "./MultilineText";
+import { Hull } from "./Hull";
 
 /**
  * 
@@ -50,46 +51,6 @@ const getBoundingBox = (points: [number, number][]): BBox => {
         height: maxY - minY
     };
 };
-
-interface HullProps {
-    id: string
-    hull: string;
-    label?: ReactNode;
-    onClick: MouseEventHandler
-    soft?: boolean
-    fillOpacity?: number
-    fill?: string
-}
-
-const Hull = ({ id, hull, onClick, label, soft, fill, fillOpacity }: HullProps) => {
-    const [hovered, setHovered] = useState(false);
-
-    return (
-        <g
-            className='hull'
-            onClick={onClick}
-        >
-            <path
-                data-id={id}
-                id={id}
-                stroke={soft ? 'none' : 'black'}
-                fill={fill || (soft ? 'gray' : 'white')}
-                fillOpacity={(fillOpacity || (soft ? 0.2 : 0.8)) + (hovered ? 0.1 : 0)}
-                strokeWidth={0.3}
-                strokeDasharray={soft ? 'none' : '5 1'}
-                d={hull}
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
-            />
-            <g style={{
-                fillOpacity: hovered ? 1 : 0.5,
-                strokeOpacity: hovered ? 1 : 0.5,
-            }}>
-                {label}
-            </g>
-        </g>
-    );
-}
 
 interface AssumptionUnderlayProps {
     assumption: AnyEditorialAssumption;
