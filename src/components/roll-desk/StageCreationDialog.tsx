0387ba3c1bb7ac5f93886e63d90320876fddb08c
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-06-13 23:20:16 +0200

    remodel stage creation dialog

--- src/components/roll-desk/StageCreationDialog.tsx
+++ src/components/roll-desk/StageCreationDialog.tsx
@@ -1,26 +1,24 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormLabel, MenuItem, Select, SelectChangeEvent, Stack, TextField } from "@mui/material"
-import { StageCreation } from "linked-rolls/lib/Stage";
-import { Edition, ObjectUsage, Stage } from "linked-rolls";
+import { AnySymbol, assign, Edition, fillEdits, Stage } from "linked-rolls";
 import { useState } from "react";
 import { v4 } from "uuid";
 
 interface StageCreationDialogProps {
     open: boolean
     edition: Edition
+    symbols: AnySymbol[]
     onClose: () => void
-    onDone: (stage: StageCreation) => void
+    onDone: (stage: Stage) => void
     clearSelection: () => void
 }
 
-export const StageCreationDialog = ({ open, edition, onClose, onDone, clearSelection }: StageCreationDialogProps) => {
+export const StageCreationDialog = ({ open, edition, symbols, onClose, onDone, clearSelection }: StageCreationDialogProps) => {
     const [siglum, setSiglum] = useState<string>('')
-    const [witnesses, setWitnesses] = useState<string[]>([])
-    const [resp, setResp] = useState<string>('')
     const [prev, setPrev] = useState<Stage>()
 
     return (
         <Dialog open={open} onClose={onClose}>
-            <DialogTitle>Edit Group Label</DialogTitle>
+            <DialogTitle>Create New Stage</DialogTitle>
             <DialogContent>
                 <Stack spacing={1} direction='column'>
                     <FormControl>
@@ -28,69 +26,45 @@ export const StageCreationDialog = ({ open, edition, onClose, onDone, clearSelec
                         <TextField value={siglum} onChange={e => setSiglum(e.target.value)} />
                     </FormControl>
                     <FormControl>
-                        <FormLabel>Witnesses</FormLabel>
-                        <Select
-                            multiple
-                            label='Witnesses'
-                            value={witnesses}
-                            onChange={(event: SelectChangeEvent<typeof witnesses>) => {
-                                const value = event.target.value
-                                setWitnesses(typeof value === 'string' ? value.split(',') : value,)
-                            }}
-                        >
-                            {edition.copies.map(copy => (
-                                <MenuItem value={copy.id} key={copy.id}>
-                                    {copy.siglum}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                    <FormControl>
                         <FormLabel>Based on stage</FormLabel>
                         <Select
                             label='Previous Stage'
-                            value={prev?.siglum || 'preliminary'}
+                            value={prev?.siglum || ''}
                             onChange={(event) => {
                                 const value = event.target.value
-                                setPrev(edition.stages.find(stage => stage.created.siglum === value)?.created)
+                                setPrev(edition.stages.find(stage => stage.siglum === value))
                             }}
                         >
                             {edition.stages.map(stage => {
-                                const siglum = stage.created.siglum
+                                const siglum = stage.siglum
                                 return (
                                     <MenuItem value={siglum} key={siglum}>
                                         {siglum}
                                     </MenuItem>
                                 )
                             })}
-                            <MenuItem value='preliminary' key={'preliminary'}>
-                                <i>Preliminary Roll</i>
+                            <MenuItem value='' key={'unknown'}>
+                                <i>No known preceding roll (first documented version)</i>
                             </MenuItem>
 
                         </Select>
-                    </FormControl>
-
-                    <FormControl>
-                        <FormLabel>Responsible</FormLabel>
-                        <TextField value={resp} onChange={e => setResp(e.target.value)} />
                     </FormControl>
                 </Stack>
             </DialogContent>
             <DialogActions>
                 <Button
                     onClick={() => {
-                        const stage = {
-                            siglum,
-                            witnesses: edition.copies.filter(copy => witnesses.includes(copy.id))
-                        }
-                        const basedOn: ObjectUsage = {
+                        const stage: Stage = {
                             id: v4(),
-                            type: 'objectUsage',
-                            original: prev || { 'id': 'preliminary' },
-                            certainty: 'likely',
+                            siglum,
+                            edits: [],
+                            intentions: [],
+                            basedOn: prev && assign('derivation', prev)
                         }
 
-                        onDone(new StageCreation(stage, basedOn))
+                        fillEdits(stage, symbols)
+
+                        onDone(stage)
                         onClose()
                         clearSelection()
                     }}
