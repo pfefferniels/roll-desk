534b987b95974dd9c518df7b64288365c86332a0
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-06-16 10:20:59 +0200

    fix IntentionView

--- src/components/roll-desk/assumptions/IntentionView.tsx
+++ src/components/roll-desk/assumptions/IntentionView.tsx
@@ -1,19 +1,8 @@
 import { Intention } from "linked-rolls";
 import { getHull, Hull } from "./Hull";
 import { getBoundingBox } from "../../../helpers/getBoundingBox";
-import { inferencesOf } from "./QuestionTree";
-import { getEditBBox, Translation } from "./EditView";
+import { getEditBBox } from "./EditView";
 import { usePinchZoom } from "../../../hooks/usePinchZoom";
-
-export const getIntentionBBox = (intention: Intention, translation: Translation) => {
-    const bboxes = inferencesOf(intention)
-        .map(inference => inference.premises)
-        .flat()
-        .filter(premise => premise.type === 'edit')
-        .map(premise => getEditBBox(premise, translation));
-
-    return getBoundingBox(getHull(bboxes).points);
-}
 
 export interface IntentionViewProps {
     intention: Intention;
@@ -23,45 +12,36 @@ export interface IntentionViewProps {
 export const IntentionView = ({ intention, onClick }: IntentionViewProps) => {
     const translation = usePinchZoom()
 
-    const hulls =
-        inferencesOf(intention)
-            .map((inference, i) => {
-                const bboxes = inference.premises
-                    .filter(premise => premise.type === 'edit')
-                    .map(premise => getEditBBox(premise, translation))
+    const bboxes = intention.belief.about
+        .map(edit => getEditBBox(edit, translation));
 
-                const { points, hull } = getHull(bboxes, 10);
-                const bbox = getBoundingBox(points);
-
-                return (
-                    <Hull
-                        key={`${intention.id}_inference_${i}`}
-                        id={`${intention.id}_inference_${i}`}
-                        data-id={`${intention.id}_inference_${i}`}
-                        hull={hull}
-                        fillOpacity={0.5}
-                        fill='red'
-                        onClick={() => {
-                            onClick && onClick(intention)
-                        }}
-                        label={
-                            <text
-                                x={bbox.x}
-                                y={bbox.y + bbox.height + 2}
-                                fontSize={10}
-                                fill='black'
-                                style={{ pointerEvents: 'none' }}
-                            >
-                                {intention.description}
-                            </text>
-                        }
-                    />
-                )
-            })
+    const hull = getHull(bboxes)
+    const bbox = getBoundingBox(hull.points)
 
     return (
         <g>
-            {...hulls}
+            <Hull
+                key={`${intention.id}`}
+                id={`${intention.id}`}
+                data-id={`${intention.id}}`}
+                hull={hull.hull}
+                fillOpacity={0.5}
+                fill='red'
+                onClick={() => {
+                    onClick && onClick(intention)
+                }}
+                label={
+                    <text
+                        x={bbox.x}
+                        y={bbox.y + bbox.height + 2}
+                        fontSize={10}
+                        fill='black'
+                        style={{ pointerEvents: 'none' }}
+                    >
+                        {intention.description}
+                    </text>
+                }
+            />
         </g>
     );
 }
